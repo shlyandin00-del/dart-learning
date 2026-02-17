044403a24e94eef3141a9719156d18ee6a3364a0
shlyandin00-del <shlyandin00@bk.ru>
2026-02-17 22:00:26 +0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  print("Hello, Sirius!");
+  print("I am ready to learn Mobile Development.");
+  
+  // Simple sum calculation
+  int a = 10;
+  int b = 20;
+  print("Sum: ${a + b}");
+}
